9701f43b1d8124b424e5b7ded25c4ab39803a05f
chylinski.t <chylinski.t@outlook.com>
2019-11-02 21:43:19 -0400

    project completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,41 +5,97 @@ generated number at the start of the game 19-120
 click on crystal add crystal number to your score  
 
 
-
 everytime u click on the crystals you get a number (if your score matches the generated number than you win)
 win++
 lose++
 score is at 0 
+if you lose or win reset game, but keep score
 */
 
-var guessNumber =  Math.floor(Math.random() * 120) + 9; 
-var winningScore1 = Math.floor(Math.random() * 12) + 1; 
-var winningScore2 = Math.floor(Math.random() * 12) + 1; 
-var winningScore3 = Math.floor(Math.random() * 12) + 1; 
-var winningScore4 = Math.floor(Math.random() * 12) + 1; 
+var guessNumber = Math.floor(Math.random() * 120) + 9;
+var winningScore1 = Math.floor(Math.random() * 12) + 1;
+var winningScore2 = Math.floor(Math.random() * 12) + 1;
+var winningScore3 = Math.floor(Math.random() * 12) + 1;
+var winningScore4 = Math.floor(Math.random() * 12) + 1;
 var wins = 0;
 var losses = 0;
+var yourTotalScore = 0;
 
-console.log('guess number ' + guessNumber);
-console.log('winning number ' +  winningScore1);
+console.log("guess number " + guessNumber);
+console.log("winning number " + winningScore1);
 
 $(document).ready(() => {
+  $("#targetScore").text(" " + guessNumber);
+  $("#winsTotal").text(" " + wins);
+  $("#lossesTotal").append(" " + losses);
+  $("#yourScore").text(" " + yourTotalScore);
 
-
-$("#targetScore").append(" " +guessNumber);
-$("#winsTotal").append(" " + wins);
-$("#lossesTotal").append(" " + losses);
-
-function win(){
-    losses = losses + 1;
+  function win() {
+    wins++;
     $("#winsTotal").text(" " + wins);
-}
+  }
 
-function lose(){
-    losses = losses + 1;
-    $("#lossesTotal").append(" " + losses);
+  function lose() {
+    losses++;
+    $("#lossesTotal").text(" " + losses);
+  }
 
-}
+  function reset() {
+    yourTotalScore = 0;
+    $("#yourScore").text(" " + yourTotalScore);
+    guessNumber = Math.floor(Math.random() * 120) + 9;
+    $("#targetScore").text(" " + guessNumber);
+    winningScore1 = Math.floor(Math.random() * 12) + 1;
+    winningScore2 = Math.floor(Math.random() * 12) + 1;
+    winningScore3 = Math.floor(Math.random() * 12) + 1;
+    winningScore4 = Math.floor(Math.random() * 12) + 1;
+  }
 
+  $("#amethyst").click(function() {
+    yourTotalScore = yourTotalScore + winningScore1;
+    $("#yourScore").text(" " + yourTotalScore);
+    if (yourTotalScore === guessNumber) {
+      win();
+      reset();
+    } else if (yourTotalScore > guessNumber) {
+      lose();
+      reset();
+    }
+  });
 
+  $("#emld").click(function() {
+    yourTotalScore = yourTotalScore + winningScore2;
+    $("#yourScore").text(" " + yourTotalScore);
+    if (yourTotalScore === guessNumber) {
+      win();
+      reset();
+    } else if (yourTotalScore > guessNumber) {
+      lose();
+      reset();
+    }
+  });
+
+  $("#ruby").click(function() {
+    yourTotalScore = yourTotalScore + winningScore2;
+    $("#yourScore").text(" " + yourTotalScore);
+    if (yourTotalScore === guessNumber) {
+      win();
+      reset();
+    } else if (yourTotalScore > guessNumber) {
+      lose();
+      reset();
+    }
+  });
+
+  $("#sapphire").click(function() {
+    yourTotalScore = yourTotalScore + winningScore2;
+    $("#yourScore").text(" " + yourTotalScore);
+    if (yourTotalScore === guessNumber) {
+      win();
+      reset();
+    } else if (yourTotalScore > guessNumber) {
+      lose();
+      reset();
+    }
+  });
 });
